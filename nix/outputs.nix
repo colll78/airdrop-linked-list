d0291a65fc71848a67b30431b509810d1f5a7883
colll78 <philipdisarro@gmail.com>
2024-10-02 13:13:08 -0700

    updat flake

--- nix/outputs.nix
+++ nix/outputs.nix
@@ -4,18 +4,6 @@ let
 in
 [
   (
-    project.flake // {
-      checks = {
-        plutarch-import-test = pkgs.runCommand "plutarch-import-test" {} ''
-          if [ -d ${pkgs.plutarch} ]; then
-            echo "Plutarch found at ${pkgs.plutarch}"
-            touch $out
-          else
-            echo "Plutarch not found"
-            exit 1
-          fi
-        '';
-      };
-    }
+    project.flake
   )
 ]
